9de0966a4a01c9327a835b219b565b8b93d2c831
franks <frank.zabala@uao.edu.co>
2020-05-18 23:03:53 -0500

    Updates

---
+++ precache-manifest.b849d0406ef0bcd15daa18597baa9806.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c14003b3f0dc532e2a51b0ecfa06ee7",
+    "url": "/Marvel-api/index.html"
+  },
+  {
+    "revision": "96d080b89ee63a59bf4b",
+    "url": "/Marvel-api/static/css/main.43ef382c.chunk.css"
+  },
+  {
+    "revision": "631eeb59d5ca03e4dc69",
+    "url": "/Marvel-api/static/js/2.4e0eec64.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Marvel-api/static/js/2.4e0eec64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d080b89ee63a59bf4b",
+    "url": "/Marvel-api/static/js/main.3aea59b7.chunk.js"
+  },
+  {
+    "revision": "dbc0dd7c3ddcced7d4a9",
+    "url": "/Marvel-api/static/js/runtime-main.3772bcfa.js"
+  },
+  {
+    "revision": "23846218973736a031ea0fd6cf36a042",
+    "url": "/Marvel-api/static/media/Logo.23846218.webp"
+  },
+  {
+    "revision": "d3d74b71ec2030076bb28ffa9972ec03",
+    "url": "/Marvel-api/static/media/buscar-negro.d3d74b71.svg"
+  },
+  {
+    "revision": "3b518cfbce14b8f2cc899968b2042865",
+    "url": "/Marvel-api/static/media/buscar-rojo.3b518cfb.svg"
+  },
+  {
+    "revision": "65d7de7c6b6bfddf28213e6b5762c834",
+    "url": "/Marvel-api/static/media/superheroes.65d7de7c.svg"
+  }
+]);
